981ce4bd6d424b044e201c3102ec1b6d71c57ac2
dharana <dharana7723@gmail.com>
2024-03-06 16:53:00 +0900

    feat: modify test

--- makeStar/streaming/tests.py
+++ makeStar/streaming/tests.py
@@ -1,5 +1,7 @@
 import json
 
+from makeStar.streaming.domain.applemusic.applemusic import AppleMusic
+
 spotify_url = "/static/spotify.json"
 spotify = json.loads(spotify_url)
 appleMusic_url = "/static/appleMusic.json"
@@ -11,25 +13,21 @@ def test_get_spotify():
 
 
 def test_get_applemusic():
-    applemusic = get_applemusic()
+    apple_music = get_applemusic()
     assert len(applemusic) != 0
 
 
-def is_converted_spotify_to_appleMusic():
-    appleMusic = convert_spotify_to_appleMusic()
+def test_is_converted_spotify_to_appleMusic():
+    apple_music = convert_spotify_to_apple_music()
     assert appleMusic.to_json == json.loads(appleMusic_url)
 
 
-def is_music_in_spotify_not_in_appleMusic():
-    appleMusic = convert_spotify_to_appleMusic(spotify)
-    assert appleMusic.to_json == json.loads(appleMusic_url)
+def test_is_music_in_spotify_but_not_exists_in_appleMusic():
+    apple_music = AppleMusic()
+    track_info = applemusic.search_apple_music_song("not exist song", "not exist artist")
+    assert track_info is None
 
 
-def is_standard_country_US():
+def test_is_standard_country_US():
     appleMusic = convert_spotify_to_appleMusic()
-    assert appleMusic.country == "US"
-
-
-from django.test import TestCase
-
-# Create your tests here.
+    assert appleMusic["track_information"]["available_countries"][0] == "US"
